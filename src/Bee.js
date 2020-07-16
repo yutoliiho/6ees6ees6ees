ab5070dffd9a9da1809e2e36d2e1f7adb4849bf5
Tong Yu <yutongsf@gmail.com>
2020-07-16 09:57:40 -0700

    complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
-class Bee {
-  // TODO..
-};
+class Bee extends Grub {
+  constructor(food) {
+    super(food);
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing'
+  }
+}
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = new Array();
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure)
+  }
 };
